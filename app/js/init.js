ccdc2df9a0cc6d879a191dfb745cbf9a7faae59b
xdrwatsonx <lider_m@mail.ru>
2017-10-28 16:03:49 +0300

    modal window started

--- app/js/init.js
+++ app/js/init.js
@@ -30,4 +30,18 @@ $( function() {
 
         return date;
     }
+
+    // modal init
+    var modalBtn = $('[data-modal]');
+
+    modalBtn.on('click', function (e) {
+
+        e.preventDefault();
+        var target = $(this).attr('data-target');
+        $(target).bPopup();
+    })
+
+    // form element styler init
+    $('select').styler();
+
 } );
--- src/js/plugin.js
+++ src/js/plugin.js
@@ -9,4 +9,39 @@
         $(this).toggleClass('open');
     });
 
+    // toggle sidebar menu
+    var menuBtn = $('.menu-btn');
+    var body = $('body');
+    var mobileClass = '_mobileSidebar';
+    var openInMobile = '_openInMobile';
+    var mobileBreakPoint = 1280;
+
+    menuBtn.on('click', function (e) {
+
+        var width = $(window).width();//получаем ширину окна
+        body.toggleClass(mobileClass);//вешаем классс для открытого меню
+
+        if ( width < mobileBreakPoint){
+            body.toggleClasss(openInMobile)
+        }
+    })
+
+    $(window).on('resize load', function (e) {
+
+        var width = $(this).width();
+        if( width < mobileBreakPoint && !body.hasClass(openInMobile) ){
+            body.addClass(mobileClass);
+        } else if( width >= mobileBreakPoint ) {
+            body.removeClass(mobileClass);
+            body.removeClass(openInMobile);
+        }
+    })
+
+    // loader hide
+    var loader = $('#loading');
+
+    $(window).on('load', function (e) {
+        loader.addClass('hide');
+    })
+
 })(jQuery);

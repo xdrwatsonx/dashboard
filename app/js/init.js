0f2e208aee7cdac406315a64d8e458b02a9dfc6b
xdrwatsonx <lider_m@mail.ru>
2017-11-01 21:35:00 +0200

    добавленн график на индексе, сделанны чекбоксы

--- app/js/init.js
+++ app/js/init.js
@@ -38,10 +38,55 @@ $( function() {
 
         e.preventDefault();
         var target = $(this).attr('data-target');
-        $(target).bPopup();
+        $(target).bPopup({
+            closeClass: 'close'
+        });
     })
 
     // form element styler init
     $('select').styler();
+
+
+    // scrollbar
+    $('.scrollbar-inner').scrollbar();
+
+    // chart init
+    var chartBlock = document.getElementById('myChart');
+    if( chartBlock ){
+        var myChart = new Chart(chartBlock, {
+            type: 'bar',
+            data: {
+
+                labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
+                datasets: [
+                    {
+                        label: 'Signups',
+                        data: [100, 200, 220, 170, 130, 90, 156, 180, 130],
+                        backgroundColor: 'rgba(128, 191, 202, 1)'
+                    },
+                    {
+                        label: 'FTD',
+                        data: [200, 120, 180, 142, 121, 90, 170, 110, 160],
+                        backgroundColor: 'rgba(255, 154, 56, 1)'
+                    },
+                    {
+                        label: 'Earned',
+                        data: [200, 120, 180, 142, 121, 90, 170, 110, 160],
+                        backgroundColor: '#59b66d'
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        })
+    }
 
 } );
